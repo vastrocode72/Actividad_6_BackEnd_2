e0805149f4c0884b1fc3ea3373fc850087ee6a8e
GGP113 <paulaag316@gmail.com>
2026-04-08 20:02:08 -0500

    creacion proyecto base

---
+++ src/main/java/com/control_de_lavanderia/control_de_lavanderia/ControlDeLavanderiaApplication.java
@@ -0,0 +1,13 @@
+package com.control_de_lavanderia.control_de_lavanderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeLavanderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeLavanderiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/control_de_lavanderia/control_de_lavanderia/ControlDeLavanderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control_de_lavanderia.control_de_lavanderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeLavanderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
